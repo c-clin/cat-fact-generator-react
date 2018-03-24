805622740877ed87999df99ddf49aa34a82f047d
Catherine Lin <catherine.lin.93@gmail.com>
2018-03-23 19:34:51 -0700

    cat facts api not working, downloaded a npm instead

--- src/App.js
+++ src/App.js
@@ -1,11 +1,94 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
+import './css/style.css';
+
+const catFacts = require('cat-facts');
+var factURL = "https://the-cat-fact.herokuapp.com/api/randomfact";
+
+// https://api.unsplash.com/photos/random?&query=cats&client_id=8a3b86c522f37241f107be57b5b3713407e5e3c59942bde90f13a62846da1106
+// unsplash cat image
+const clientID = "8a3b86c522f37241f107be57b5b3713407e5e3c59942bde90f13a62846da1106"
+const query = 'cats'
+var imageURL = `https://api.unsplash.com/photos/random?&query=${query}&client_id=${clientID}`;
+let randomFact = catFacts.random();
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      resultFact: [],
+      resultCat: [],
+      resultUser: []
+    };
+    this.getCat = this.getCat.bind(this);
+  }
+
+  getCatFact() {
+    
+    console.log(randomFact);
+    this.setState({resultFact: randomFact})
+    // $.ajax({
+    //   url: factURL,
+    //   type: 'GET',
+    //   // dataType: 'jsonp',
+    //   // jsonp: false,
+    //   // crossDomain: true,
+    //   contentType: 'text/plain',
+    //   xhrFields: {
+    //     withCredentials: false
+    //   },
+    //   headers: {
+    //             // "Accept" : "application/javscript",
+    //             // "Content-Type": "application/javascript",
+    //             // "Access-Control-Allow-Origin" : "*",
+    //             "Access-Control-Allow-Header" : "header"
+    //   },
+    //   cashe: false,
+    //   success: function(data) {
+    //     var fact = data.data.fact;
+    //    console.log(fact);
+    //     this.setState({resultFact: fact});
+    //   }.bind(this),
+    //   error: function(error) {
+    //    console.log('fail(): ' + error);
+    //   }.bind(this)
+    // });
+  }
+
+
+
+  getCat() {
+    $.getJSON(imageURL).done(function(data) {
+      var resultPhoto = data.urls.small;
+      var user = data.user.name;
+      this.setState({resultCat: resultPhoto, resultUser: 'Awesome photo by: ' + user});
+    }.bind(this))
+    .fail(function(error) {
+      throw new Error('fail(): ' + error);
+    })
+  }  
+
+  onClick() {
+    this.getCatFact();
+    this.getCat();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Random Cat Fact Generator</h1>
+
+
+          <div className="unsplash">
+            <button onClick={this.onClick.bind(this)}>get cat!</button>
+            <h2>{this.state.resultFact}</h2>
+            <figure className="randomCatPhoto">
+              <img src={this.state.resultCat} />
+              <figcaption>{this.state.resultUser}</figcaption>
+            </figure>
+          </div>        
       </div>
     );
   }
